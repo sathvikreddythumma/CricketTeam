22d0d3fc85c22fcc7fe8c63ec84af5ef4caedbd8
sathvikreddythumma <sathvikreddythumma@gmail.com>
2023-09-05 10:38:07 +0000

    solution

--- app.js
+++ app.js
@@ -21,60 +21,75 @@ const initializeDBAndServer = async () => {
   }
 };
 initializeDBAndServer();
-
+//API 1
 app.get("/players/", async (request, response) => {
   const getBookQuery = `SELECT * FROM cricket_team;`;
   const playersArray = await db.all(getBookQuery);
-  //  response.send(booksArray);
-  response.send(
-    playersArray.map((eachPlayer) =>
-      convertDbObjectToResponseObject(eachPlayer)
-    )
-  );
+  const ans = (playersArray) => {
+    return {
+      playerId: playersArray.player_id,
+      playerName: playersArray.player_name,
+      jerseyNumber: playersArray.jersey_number,
+      role: playersArray.role,
+    };
+  };
+  response.send(playersArray.map((each) => ans(each)));
 });
-
+//API 2
 app.use(express.json());
 app.post("/players/", async (request, response) => {
   const plaDetails = request.body;
-  const { player_name, jersey_number, role } = plaDetails;
+  const { playerName, jerseyNumber, role } = plaDetails;
   const addPlyDetails = `insert into cricket_team
   (player_name,jersey_number,role)
   values
-  ('${player_name}',
-   '${jersey_number}',
+  ('${playerName}',
+   '${jerseyNumber}',
    '${role}');`;
-  const dbRes = await db.run(addPlyDetails);
-  const player_id = dbRes.lastID;
-  response.send({ player_id: player_id });
-});
+  const playersArray = await db.run(addPlyDetails);
+  const player_id = playersArray.lastID;
+  {
+    player_id: player_id;
+  }
 
+  response.send(playersArray);
+});
+//API 3
 app.get("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const getPly = `SELECT * FROM cricket_team where player_id='${playerId}';`;
   const plaArray = await db.get(getPly);
   response.send(plaArray);
 });
-
+//API 4
 app.put("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const plaDetails = request.body;
-  const { player_name, jersey_number, role } = plaDetails;
+  const { playerName, jerseyNumber, role } = plaDetails;
   const upPlyDetails = `UPDATE  cricket_team SET
  
-  player_name='${player_name}',
-   jersey_number='${jersey_number}',
+  player_name='${playerName}',
+   jersey_number='${jerseyNumber}',
    role='${role}'
     WHERE
-      player_id = ${playerId};`;
-  const plaArray = await db.run(upPlyDetails);
-  response.send(plaArray);
+      player_id = '${playerId}';`;
+  const playersArray = await db.run(upPlyDetails);
+  const ans = (playersArray) => {
+    return {
+      playerId: playersArray.player_id,
+      playerName: playersArray.player_name,
+      jerseyNumber: playersArray.jersey_number,
+      role: playersArray.role,
+    };
+  };
+  response.send(playersArray);
 });
-
+//API 5
 app.delete("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const getPly = `DELETE FROM cricket_team where player_id= ${playerId};`;
   const plaArray = await db.run(getPly);
-  response.send(plaArray);
+  response.send("Player Removed");
 });
 
 const convertDbObjectToResponseObject = (dbObject) => {
